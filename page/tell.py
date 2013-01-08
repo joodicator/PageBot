9789ec516b493f965621d9827df1849fccf0878b
joo <john.macinroe@gmail.com>
2013-01-08 00:54:49 +0000

    Changed !tell so that: wildcards can be used in the recipient nick; a full hostmask can be specified instead of a nick; messages are only delivered on speech, as opposed to also upon joining.

--- page/tell.py
+++ page/tell.py
@@ -62,8 +62,15 @@ def reply(bot, id, target, msg, prefix=True):
 @link('COMMAND_LIST')
 def list_tell(bot, reply):
     reply('tell NICK MESSAGE',
-    'When NICK is next seen in this channel,'
-    ' MESSAGE will be delivered to them.')
+    'When NICK is next seen in this channel, MESSAGE will be delivered to them.'
+    ' NICK is matched case-insensitively against the nick of anybody appearing'
+    ' in the channel.')
+    reply(None,
+    'If NICK contains the characters * or ?, these will match any sequence of'
+    ' of zero or more characters, or exactly 1 character, respectively.')
+    reply(None,
+    'If NICK contains any occurrence of ! or @, it will be matched against the'
+    ' full NICK!USER@HOST of the recipient, instead of just their nick.')
 
 @link('!tell')
 def h_tell(bot, id, target, args, full_msg):
@@ -72,8 +79,13 @@ def h_tell(bot, id, target, args, full_msg):
             'Error: the "tell" command may only be used in a channel.')
         return
 
-    state = get_state()
     to_nick, msg = re.match(r'(\S+)\s+(.*)', args).groups()
+    if to_nick.startswith('#'):
+        reply(bot, id, target,
+            'Error: "%s" is not a valid nick or hostmask.' % to_nick)
+        return
+
+    state = get_state()
     record = Message(
         time_sent   = datetime.datetime.utcnow(),
         channel     = target,
@@ -113,11 +125,11 @@ def h_tell_reset(bot, id, target, args, full_msg):
     put_state(State())
     reply(bot, id, target, 'Done.')
 
-
+'''
 @link('OTHER_JOIN')
 def h_other_join(bot, id, chan):
     report(bot, id, chan)
-
+'''
 
 @link('MESSAGE')
 def h_message(bot, id, target, msg):
@@ -135,7 +147,8 @@ def report(bot, id, chan):
 # `msg', possibly notifying `id' of this message, in which case returns True;
 # otherwise returns False.
 def report_msg(bot, id, chan, msg):
-    if (msg.to_nick, msg.channel) != (id.nick, chan): return False
+    if msg.channel != chan: return False
+    if not msg_matches_id(msg, id): return False
     delta = datetime.datetime.utcnow() - msg.time_sent
     if delta.total_seconds() < 1: return False
     d_mins, d_secs = divmod(delta.seconds, 60)
@@ -147,3 +160,20 @@ def report_msg(bot, id, chan, msg):
         '%sd, %02d:%02d:%02d' % (delta.days, d_hours, d_mins, d_secs)))
     bot.send_msg(chan, "<%s> %s" % (msg.from_id.nick, msg.message))
     return True
+
+# Returns True if the given message matches the given user ID as a recipient,
+# or otherwise returns False.
+def msg_matches_id(msg, id):
+    str = '%s!%s@%s' % tuple(id) if re.search(r'!|@', msg.to_nick) else id.nick
+    pat = wc_to_re(msg.to_nick)
+    print 'DEBUG: str=%s; pat=%s' % (str, pat)
+    return re.match(pat, str, re.I) != None
+
+# Returns a Python regular expression pattern string equivalent to the given
+# wildcard pattern (which accepts only the entire input, not part of it).
+def wc_to_re(wc):
+    def sub(match):
+        if match.group(1): return '.*'
+        elif match.group(2): return '.'
+        else: return re.escape(match.group(3))
+    return '^' + re.sub(r'(\*)|(\?)|([^*?]+)', sub, wc) + '$'
