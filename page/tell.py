080702cda603fc442089a7950e2b35cdb65da0c1
Joseph Crowe <joseph.crowe@live.co.uk>
2013-02-26 17:56:10 +0000

    Ajustment to message notification logic.

--- page/tell.py
+++ page/tell.py
@@ -197,15 +197,24 @@ def h_tell_reset(bot, id, target, args, full_msg):
 def h_other_join(bot, id, chan):
     notify_msgs(bot, id, chan)
 
-@link('NICK')
-def h_nick(bot, id, new_nick, *args):
-    id = util.ID(*id)
-    new_id = util.ID(new_nick, id.user, id.host)
-    yield untwisted.magic.sign('tell.notify_msgs_all', bot, new_id)
-
 @link('MESSAGE')
 def h_message(bot, id, target, msg):
     if target: deliver_msgs(bot, id, target)
+
+@link('NICK')
+def h_nick(bot, id, new_nick, *args):
+    state = get_state()
+    old_id = util.ID(*id)
+    new_id = util.ID(new_nick, old_id.user, old_id.host)
+    for chan in {m.channel for m in state.msgs}:
+        names = yield channel.names(bot, chan)
+        names = map(str.lower, channel.strip_names(names))
+        if new_nick.lower() not in names: continue
+        new_msgs = {m for m in state.msgs
+                    if would_deliver(new_id, chan, m)
+                    and not would_deliver(old_id, chan, m)}
+        if not new_msgs: continue
+        notify_msgs(bot, new_id, chan)
 
 # Notify `id' of messages left for them in `chan', if any.
 def notify_msgs(bot, id, chan):
@@ -216,18 +225,7 @@ def notify_msgs(bot, id, chan):
             'You have %s messages; say anything to read them.' % len(msgs))
     elif len(msgs):
         reply(bot, id, chan,
-            'You have a message; say anything to read it.')
-
-# As notify_msgs, but acts on all channels in which id resides.
-@link('tell.notify_msgs_all')
-def h_notify_msgs_all(bot, id):
-    nick = id.nick.lower()
-    state = get_state()
-    for chan in {m.channel for m in state.msgs}:
-        names = yield channel.names(bot, chan)
-        names = map(str.lower, channel.strip_names(names))
-        if nick not in names: continue
-        notify_msgs(bot, id, chan)
+            'You have a message; say anything to read it.')    
 
 # Deliver to `id' any messages left for them in `chan'.
 def deliver_msgs(bot, id, chan):
