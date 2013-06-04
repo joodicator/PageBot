dacb34f2e2bb2a6e08c32277e1b0a5884c2ae9f2
Joseph Crowe <joseph.crowe@live.co.uk>
2013-06-04 19:17:53 +0100

    Added Terraria protocol implementation.

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python2
 
 import sys, os, os.path
-os.chdir(os.path.dirname(sys.argv[0]))
+if sys.argv[0]:
+    os.chdir(os.path.dirname(sys.argv[0]))
 sys.path[0:0] = ['.', 'ameliabot', 'page', 'lib']
 
-conf = {
-    'server':   'localhost',
-    'port':     6667,
-    'nick':     'Page',
-    'user':     'Page',
-    'name':     'https://github.com/JosephCrowe/page-ircbot',
-    'channels': ['#page'],
-    'plugins':  ['runtime', 'message', 'nickserv', 'auth', 'control',
-                 'channel', 'tell']
-}
-execfile('conf/bot.py', conf)
+if __name__ == '__main__':
+    conf = {
+        'server':   'localhost',
+        'port':     6667,
+        'nick':     'Page',
+        'user':     'Page',
+        'name':     'https://github.com/JosephCrowe/page-ircbot',
+        'channels': ['#page'],
+        'plugins':  ['runtime', 'message', 'nickserv', 'auth', 'control',
+                     'channel', 'tell']
+    }
+    execfile('conf/bot.py', conf)
+    
+    if '--debug' in sys.argv: conf['plugins'].insert(0, 'debug')
+    
+    
+    import amelia
+    from untwisted.event import CLOSE
+    
+    bot = amelia.AmeliaBot(conf)
+    bot.domain = 'bot'
+    try:
+        bot.mainloop()
+    except BaseException as e:
+        bot.drive('EXCEPTION', bot, e)
+        bot.mainloop()
 
-if '--debug' in sys.argv: conf['plugins'].insert(0, 'debug')
-
-
-import amelia
-from untwisted.event import CLOSE
-
-bot = amelia.AmeliaBot(conf)
-bot.domain = 'bot'
-try:
-    bot.mainloop()
-except BaseException as e:
-    bot.drive('EXCEPTION', bot, e)
-    bot.mainloop()
