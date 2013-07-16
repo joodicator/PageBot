88da4ed466b14bbbcc8d9fd15609c50a9df56f43
joo <john.macinroe@gmail.com>
2013-07-16 14:39:11 +0100

    Added !dismiss and !undismiss commands to the tell plugin.

--- page/tell.py
+++ page/tell.py
@@ -1,10 +1,6 @@
-# Possible future changes:
-#   - Require messages given upon joining a channel to be explicitly dismissed.
-#     OR only notify users of the presence of messages, requiring them to
-#     retrieve their messages manually.
-#   - Display only 3 messages at once, delivering further messages by PM.
-#   - Allow each USER@HOST sender to leave at most 1 message for each RECIPIENT.
-#   - Allow a USER@HOST sender to edit or delete their last message to any RECIPIENT.
+#==============================================================================#
+# tell.py - provides !tell, and related commands, to allow users to leave each
+# other messages in an IRC channel.
 
 import util
 import auth
@@ -22,7 +18,12 @@ import datetime
 import time
 import re
 
+#==============================================================================#
 link, install, uninstall = LinkSet().triple()
+
+# The number of days from a message's timestamp after which it may be deleted
+# from the dismissed_msgs list.
+DISMISS_DAYS = 30
 
 # The file where the plugin's persistent state is stored.
 STATE_FILE = 'state/tell.pickle'
@@ -30,6 +31,19 @@ STATE_FILE = 'state/tell.pickle'
 # The memory-cached persistent global state.
 STATE = None
 
+# A saved message kept by the system.
+Message = namedtuple('Message',
+    ('time_sent', 'channel', 'from_id', 'to_nick', 'message'))
+
+# The plugin's persistent state object.
+class State(object):
+    def __init__(self):
+        self.msgs = []
+        self.dismissed_msgs = []
+    def __getinitargs__(self):
+        return ()
+
+#==============================================================================#
 # Load the plugin's persistent state object.
 def get_state():
     # We use the in-memory state if it exists, to avoid excessive file IO.
@@ -55,17 +69,7 @@ def put_state(state):
         pickler.clear_memo()
         pickler.dump(state)
 
-# The plugin's persistent state object.
-class State(object):
-    def __init__(self):
-        self.msgs = []
-    def __getinitargs__(self):
-        return ()
-
-# A saved message kept by the system.
-Message = namedtuple('Message',
-    ('time_sent', 'channel', 'from_id', 'to_nick', 'message'))
-
+#==============================================================================#
 @link('HELP')
 def h_help_tell_short(bot, reply, args):
     reply('tell NICK MESSAGE',
@@ -75,14 +79,10 @@ def h_help_tell_short(bot, reply, args):
 def h_help_tell(bot, reply, args):
     h_help_tell_short(bot, reply, args)
     reply('',
-    'NICK is matched case-insensitively against the nick of anybody appearing'
-    ' in the channel.')
-    reply('',
-    'If NICK contains the characters * or ?, these will match any sequence of'
-    ' zero or more characters, or exactly 1 character, respectively.')
-    reply('',
-    'If NICK contains any occurrence of ! or @, it will be matched against the'
-    ' full NICK!USER@HOST of the recipient, instead of just their nick.')
+    'If NICK contains any occurrence of ! or @, it will be matched against'
+    ' the full NICK!USER@HOST of the recipient, instead of just their nick;'
+    ' if NICK contains the wildcard characters * or ?, these will match any'
+    ' sequence of 0 or more characters, or exactly 1 character, respectively.')
 
 @link('!tell')
 def h_tell(bot, id, target, args, full_msg):
@@ -116,6 +116,7 @@ def h_tell(bot, id, target, args, full_msg):
     put_state(state)
     reply(bot, id, target, 'It shall be done.')
 
+#==============================================================================#
 @link('HELP')
 def h_help_untell_short(bot, reply, args):
     reply('untell NICK',
@@ -167,7 +168,76 @@ def h_untell(bot, id, target, args, full_msg):
     reply(bot, id, target, '%s %s deleted.' %
         (count, 'message' if count == 1 else 'messages'))
 
+#==============================================================================#
+@link('HELP')
+def h_help_dismiss_short(bot, reply, args):
+    reply('dismiss [NICK]',
+    'Cancels delivery of the last message left for you.')
 
+@link(('HELP', 'dismiss'))
+def h_help_dismiss(bot, reply, args):
+    reply('dismiss [NICK]',
+    'If NICK is given, dismisses the most recent message left for you by NICK,'
+    ' preventing it from being delivered; otherwise, dismisses the most recent'
+    ' message left by anybody. Messages may be recovered using "undismiss".',
+    'NICK may be an IRC nick or a NICK!USER@HOST, and may contain the wildcard'
+    ' characters * and ?, as specified in "help tell", in which case the last'
+    ' matching message is dismissed.')
+
+@link('!dismiss')
+def h_dismiss(bot, id, target, query, *args):
+    state = get_state()
+    msgs = [m for m in state.msgs if not query or match_id(query, m.from_id)]
+    if not msgs: return reply(bot, id, target,
+        'You have no messages%s to dismiss.' % (query and ' from "%s"' % query))
+    msg = msgs[-1]
+    state.msgs.remove(msg)
+    state.dismissed_msgs = [m for m in state.dismissed_msgs
+        if (datetime.datetime.utcnow() - m.time_sent).days <= DISMISS_DAYS]
+    state.dismissed_msgs.append(msg)
+    put_state(state)
+    count = len(state.msgs)
+    reply(bot, id, target,
+        '1 message from %s deleted; you now have %s message%s'
+        ' (you may reverse this using "undismiss").'
+        % (msg.from_id.nick, count, 's' if count != 1 else ''))
+
+#==============================================================================#
+@link('HELP')
+def h_help_undismiss_short(bot, reply, args):
+    reply('undismiss [NICK]',
+    'Restores the last message that you dismissed.')
+
+@link(('HELP', 'undismiss'))
+def h_help_undismiss(bot, reply, args):
+    reply('undismiss [NICK]',
+    'Reverses the effect of "dismiss", restoring the last dismissed message'
+    ' from NICK, or from anybody if NICK is not specified. This may be done'
+    ' multiple times to restore messages from up to %s days ago.'
+    % DISMISS_DAYS,
+    'As with "dismiss", NICK may take the form NICK!USER@HOST, and may contain'
+    ' the wildcard characters * and ?.')
+
+@link('!undismiss')
+def h_undismiss(bot, id, target, query, *args):
+    state = get_state()
+    msgs = [m for m in state.dismissed_msgs
+        if not query or match_id(query, m.from_id)]
+    if not msgs: return reply(bot, id, target,
+        'You have no dismissed messages%s.'
+        % (query and ' from "%s"' % query))
+    msg = msgs[-1]
+    state.dismissed_msgs.remove(msg)
+    state.msgs.append(msg)
+    put_state(state)
+    count = len(state.msgs)
+    reply(bot, id, target,
+        '1 message from %s restored; you now have %s message%s'
+        ' (say anything to read %s).'
+        % (msg.from_id.nick, count, 's' if count != 1 else '',
+        'them' if count != 1 else 'it'))
+
+#==============================================================================#
 @link('!tell_list')
 @admin
 def h_tell_list(bot, id, target, args, full_msg):
@@ -187,12 +257,14 @@ def h_tell_list(bot, id, target, args, full_msg):
     map(output, lines[1:])
     output('\2End of tell_list')
 
+#==============================================================================#
 @link('!tell_reset')
 @admin
 def h_tell_reset(bot, id, target, args, full_msg):
     put_state(State())
     reply(bot, id, target, 'Done.')
 
+#==============================================================================#
 @link('OTHER_JOIN')
 def h_other_join(bot, id, chan):
     notify_msgs(bot, id, chan)
@@ -211,6 +283,7 @@ def h_nick(bot, id, new_nick, chan):
                 and not would_deliver(old_id, chan, m)}
     if new_msgs: notify_msgs(bot, new_id, chan)
 
+#==============================================================================#
 # Notify `id' of messages left for them in `chan', if any.
 def notify_msgs(bot, id, chan):
     state = get_state()
@@ -222,6 +295,7 @@ def notify_msgs(bot, id, chan):
         reply(bot, id, chan,
             'You have a message; say anything to read it.')    
 
+#==============================================================================#
 # Deliver to `id' any messages left for them in `chan'.
 def deliver_msgs(bot, id, chan):
     state = get_state()
@@ -234,34 +308,41 @@ def deliver_msgs(bot, id, chan):
     state.msgs = msgs_keep
     put_state(state)
 
-# Unconditionally deliver `msg' to `id' in `chan'.
+#==============================================================================#
+# Unconditionally deliver `msg' to `id' in `chan', or by PM if `chan' is None.
 def deliver_msg(bot, id, chan, msg):
     delta = datetime.datetime.utcnow() - msg.time_sent
     if delta.total_seconds() < 1: return False
     d_mins, d_secs = divmod(delta.seconds, 60)
     d_hours, d_mins = divmod(d_mins, 60)
+
     bot.send_msg(chan, '%s: %s said on %s UTC (%s ago):' % (
         id.nick,
         '%s!%s@%s' % tuple(msg.from_id),
         msg.time_sent.strftime('%d %b %Y, %H:%M'),
         '%sd, %02d:%02d:%02d' % (delta.days, d_hours, d_mins, d_secs)))
     bot.send_msg(chan, "<%s> %s" % (msg.from_id.nick, msg.message))
+
     return True
 
+#==============================================================================#
 # Returns True if `msg' would be delivered at this time to `id' in `chan',
 # or otherwise returns False.
 def would_deliver(id, chan, msg):
     if msg.channel != chan: return False
-    
-    str = '%s!%s@%s' % tuple(id) if re.search(r'!|@', msg.to_nick) else id.nick
-    pat = wc_to_re(msg.to_nick)
-    if re.match(pat, str, re.I) == None: return False
-
+    if not match_id(msg.to_nick, id): return False
     delta = datetime.datetime.utcnow() - msg.time_sent
     if delta.total_seconds() < 1: return False    
-    
     return True
 
+#==============================================================================#
+# Returns True if `query', which is is a wildcard expression matching either a
+# nick or a nick!user@host, matches the given id.
+def match_id(query, id):
+    id_str = '%s!%s@%s' % tuple(id) if re.search(r'!|@', query) else id.nick
+    return re.match(wc_to_re(query), id_str, re.I) is not None
+
+#==============================================================================#
 # Returns a Python regular expression pattern string equivalent to the given
 # wildcard pattern (which accepts only the entire input, not part of it).
 def wc_to_re(wc):
