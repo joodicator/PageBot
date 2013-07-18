b5a8a8ddec83e5dbae176a7c03fb99f9babd92da
joo <john.macinroe@gmail.com>
2013-07-18 12:19:20 +0100

    Fixed bugs in tell's unpickling of state.

--- lib/untwisted/mode.py
+++ lib/untwisted/mode.py
@@ -7,13 +7,13 @@ from usual import *
 
 class Mode(object):
     def __init__(self, default=void):
-        """ Constructor for Mode """
+        # Constructor for Mode
 
         self.base = OrderedDict()
         self.default = default
     
     def drive(self, event, *args):
-        """ It evaluates all callbacks linked to event """
+        # It evaluates all callbacks linked to event
 
         for signal, handle in self.base.keys():
             ################
@@ -29,26 +29,24 @@ class Mode(object):
                     seq = handle(*new_args)
 
                     if seq:
-                        chain(self, seq)        
-                except Exception as excpt:
-                    #it stops processing event
-                    if isinstance(excpt, Stop):
-                        break
-                    elif isinstance(excpt, Kill):
-                        raise
-                    else:
-                        traceback.print_exc()
+                        chain(self, seq)
+                except Stop:
+                    break
+                except Exception:
+                    traceback.print_exc()
+                except Kill:
+                    raise
 
              ################
 
         self.default(event, *args)
 
     def link(self, event, callback, *args):
-        """ This function maps an event to a callback. """
+        # This function maps an event to a callback.
 
         self.base[event, callback] = args
 
     def unlink(self, event, callback, *args):
-        """ This function unmap an event to a callback. """
+        # This function unmap an event to a callback.
 
         del self.base[event, callback]
