09131c406e2f1e2f32318fe05f0fd6c61217d367
joo <john.macinroe@gmail.com>
2013-10-08 19:15:55 +0100

    Removed PM command format with no prefix. Changed kakasi to accept plain messages by PM.

--- page/kakasi.py
+++ page/kakasi.py
@@ -15,8 +15,8 @@ with open('conf/kakasi.txt') as file:
 #===============================================================================
 @link('MESSAGE')
 def h_message(bot, id, target, msg):
-    if (target and target.lower()) not in auto_targets: return
-    if kakasi_lib.is_ja(msg): kakasi(bot, id, target, msg)
+    if (target or 'PRIVMSG').lower() not in auto_targets: return
+    if kakasi_lib.is_ja(msg): kakasi(bot, id, target, msg, target is not None)
 
 #===============================================================================
 @link('HELP')
@@ -25,16 +25,18 @@ def h_help(bot, reply, args):
     'Converts Japanese characters to romaji.')
 
 @link(('HELP', 'romaji'))
+@link(('HELP', 'rj'))
 def h_help_romaji(bot, reply, args):
     reply('romaji TEXT')
     reply('rj TEXT',
     'Shows the Hepburn romanisation(s) of Japanese characters occurring in TEXT,'
-    ' according to KAKASI <http://kakasi.namazu.org>.')
+    ' according to KAKASI <http://kakasi.namazu.org>. The command prefix may be'
+    ' omitted, leaving just TEXT, if the majority of TEXT is Japanese.')
 
 @link('!romaji')
 @link('!rj')
 def h_romaji(bot, id, target, args, full_msg):
-    kakasi(bot, id, target, args, prefix=False)
+    kakasi(bot, id, target, args, prefix=target is not None)
 
 #===============================================================================
 def kakasi(bot, id, target, msg, prefix=True):
