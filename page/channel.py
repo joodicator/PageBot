e2f95b2af251a92ffab8cfff63b2377b765aecca
Joseph Crowe <joseph.crowe@live.co.uk>
2013-10-24 03:00:37 +0100

    Fix !online command for terraria and channels.

--- page/channel.py
+++ page/channel.py
@@ -27,7 +27,7 @@ def h_names(bot, chan):
 @link('353')
 def h_rpl_namereply(bot, _1, _2, _3, chan, names):
     names = re.findall(r'\S+', names)
-    names_channels[chan] += names
+    names_channels[chan.lower()] += names
 
     track_names = track_channels[chan.lower()]
     for name in strip_names(names):
@@ -37,17 +37,17 @@ def h_rpl_namereply(bot, _1, _2, _3, chan, names):
 
 @link('366')
 def h_rpl_endofnames(bot, _1, _2, chan, *args):
-    yield sign(('channel.names', bot, chan.lower()), names_channels[chan])
-    del names_channels[chan]
+    yield sign(('channel.names', bot, chan.lower()), names_channels[chan.lower()])
+    del names_channels[chan.lower()]
 
 
 @link('SOME_JOIN')
 def h_self_join(bot, id, chan):
     chan = chan.lower()
-    names = track_channels[chan]
+    names = track_channels[chan.lower()]
     if id.nick.lower() in map(str.lower, names): return
     names.append(id.nick)
-    track_channels[chan] = names
+    track_channels[chan.lower()] = names
 
 @link('SELF_PART')
 @link('SELF_KICKED')
@@ -58,16 +58,16 @@ def h_self_part_kicked_quit(bot, chan, *args):
 @link('OTHER_PART')
 def h_other_part(bot, id, chan, *args):
     chan = chan.lower()
-    names = track_channels[chan]
+    names = track_channels[chan.lower()]
     names = [n for n in names if n.lower() != id.nick.lower()]
-    track_channels[chan] = names
+    track_channels[chan.lower()] = names
 
 @link('OTHER_KICKED')
 def h_other_kicked(bot, nick, op_id, chan, *args):
     chan = chan.lower()
-    names = track_channels[chan]
+    names = track_channels[chan.lower()]
     names = [n for n in names if n.lower() != nick.lower()]
-    track_channels[chan] = names
+    track_channels[chan.lower()] = names
 
 @link('SELF_QUIT')
 def h_self_quit(bot, msg):
@@ -80,7 +80,7 @@ def h_other_quit(bot, id, msg):
     for chan, names in track_channels.iteritems():
         if id.nick.lower() not in map(str.lower, names): continue
         names = [n for n in names if n.lower() != id.nick.lower()]  
-        track_channels[chan] = names
+        track_channels[chan.lower()] = names
         yield sign('OTHER_QUIT_CHAN', bot, id, chan, msg)
         yield sign(('OTHER_QUIT_CHAN', chan), bot, id, msg)
 
@@ -90,7 +90,7 @@ def h_other_nick(bot, id, new_nick):
     for chan, names in track_channels.iteritems():
         if old_nick not in map(str.lower, names): continue
         names = map(lambda n: new_nick if n.lower() == old_nick else n, names)
-        track_channels[chan] = names
+        track_channels[chan.lower()] = names
         yield sign('SOME_NICK_CHAN', bot, id, new_nick, chan)
         yield sign(('SOME_NICK_CHAN', chan), bot, id, new_nick)
         if old_nick != bot.nick.lower():
