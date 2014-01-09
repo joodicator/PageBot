ec8ac267e334a20175b5cba2cdec58ee8101d5bb
joo <john.macinroe@gmail.com>
2014-01-09 12:38:35 +0000

    Add chanserv.py.

---
+++ page/chanserv.py
@@ -0,0 +1,33 @@
+import channel
+import util
+
+link, l_install, uninstall = util.LinkSet().triple()
+
+EVICT_FILE      = 'conf/chanserv_evict.txt'
+PASSWORD_FILE   = 'conf/chanserv_password.py'
+
+def install(bot):
+    l_install(bot)
+    for chan in channel.track_channels.iterkeys():
+        evict_chanserv(bot, chan)
+
+@link('OTHER_JOIN')
+def h_other_join(bot, id, chan):
+    evict_chanserv(bot, chan)
+
+@link('NAMES')
+def h_names(bot, chan):
+    evict_chanserv(bot, chan)
+
+def evict_chanserv(bot, chan):
+    with open(EVICT_FILE) as file:
+        evict_channels = re.findall(r'\S+', file.read())
+    if chan not in evict_channels: return
+
+    names = map(str.lower, channel.track_channels[chan])
+    if 'ChanServ'.lower() not in names: return
+
+    records = util.table(PASSWORD_FILE)
+    for r in [r for r in records if r.channel.lower() == chan]:
+        bot.send_msg('ChanServ', 'IDENTIFY %s %s' % (chan, r.password))
+        bot.send_msg('ChanServ', 'PART %s' % chan)
