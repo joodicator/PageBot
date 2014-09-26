84aa0b5cb1c427aca7ac548dc7e28e31d40df6e7
Joseph Crowe <john.macinroe@gmail.com>
2014-09-26 04:27:26 +0100

    Add output flood protection.

--- ameliabot/utils/misc.py
+++
@@ -1,8 +0,0 @@
-def send_msg(server, target, msg, **kwds):
-    server.dump('PRIVMSG %s :%s\r\n' % (target, msg))
-    server.drive('SEND_MSG', server, target, msg, kwds)
-    server.activity = True
-
-def send_cmd(server, cmd):
-    server.dump('%s\r\n' % cmd)
-    server.activity = True
