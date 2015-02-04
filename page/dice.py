a57e348140e982736393884763c896a05eb1d252
joo <john.macinroe@gmail.com>
2015-02-04 02:07:22 +0000

    Add weighted choice feature to !roll command.

--- page/dice.py
+++ page/dice.py
@@ -1,5 +1,6 @@
 import re
 import random
+import math
 
 from untwisted.magic import sign
 
@@ -20,13 +21,19 @@ def h_help(bot, reply, args):
 #===============================================================================
 @link(('HELP', 'roll'), ('HELP', 'r'))
 def h_help_roll(bot, reply, args):
-    reply('roll MdN[+K|-K] ...',
+    reply('roll ... MdN[+K|-K] ...',
     'Simulates one or more dice rolls. '
     'A roll is specified as "MdN", "MdN+K", or "MdN-K", where M is the number'
     ' of dice, N is the number of sides of each die, and K or -K is an integer'
     ' added to the result. '
     'Rolls may be annotated with other text, which is repeated in the results. '
     'The shorthand "!r" may be used instead of "!roll".')
+    reply('roll {[WEIGHT1:]ITEM1, [WEIGHT2:]ITEM2, ...} ...',
+    'For each comma-separated list of items enclosed in curly braces, chooses'
+    ' one item at random and replaces the list with that item. Items are'
+    ' selected with probability proportional to their WEIGHT, which can be'
+    ' specified as a positive number followed by a colon before the item, or'
+    ' otherwise defaults to 1.')
 
 #===============================================================================
 @link('!r', '!d')
@@ -34,10 +41,11 @@ def h_help_roll(bot, reply, args):
 def h_roll_abbrev(bot, id, target, args, full_msg):
     return h_roll(bot, id, target, args, full_msg)
 
+class UserError(Exception):
+    pass
+
 @link('!roll', '!dice')
 def h_roll(bot, id, target, args, full_msg):
-    class UserError(Exception):
-        pass
 
     rolls = []
 
@@ -62,15 +70,58 @@ def h_roll(bot, id, target, args, full_msg):
         rstr, rint = roll_str_int(dice, sides, add)
         rolls.append(((dice, sides, add), rint))
         return rstr
-    
+
     try:
-        msg = re.sub(r'\b(\d*)[dD](\d+)([+-]\d+)?\b', roll_sub, args)
-        if msg == args: raise UserError('No dice rolls specified.')
+        msg = expand_choices(args)
+        msg = re.sub(r'\b(\d*)[dD](\d+)([+-]\d+)?\b', roll_sub, msg)
+        if msg == args: raise UserError('No dice rolls or choices specified.')
         if len(msg) > 400: msg = '%s(...)' % msg[:395]
         yield sign('DICE_ROLLS', bot, id, target, rolls, msg)
         message.reply(bot, id, target, msg)
     except UserError as e:
         message.reply(bot, id, target, 'Error: %s' % e.message)
+
+#===============================================================================
+def expand_choices(str):
+    while True:
+        match = re.search(r'\{(?P<items>[^{},]*,[^{}]*)\}', str)
+        if not match: break
+        weight_sum = 0
+        cum_weight_items = []
+
+        for item in match.group('items').split(','):
+            item = item.strip()
+            pmatch = re.match(r'(?P<weight>\d*.?\d+)\s*:\s*(?P<item>.*)', item)
+            if pmatch:
+                weight = float(pmatch.group('weight'))
+                if weight <= 0: raise UserError('The weight "%s" is too small.'
+                    % abbrev_middle(pmatch.group('weight')))
+            if pmatch:
+                item = pmatch.group('item')
+            else:
+                weight = 1
+            weight_sum += weight
+            cum_weight_items.append((weight_sum, item))
+
+        if math.isinf(weight_sum): raise UserError(
+            'The weights in "%s" are too large.' % abbrev_middle(match.group()))
+        if math.isnan(weight_sum): raise UserError(
+            'The weights in "%s" are invalid.' % abbrev_middle(match.group()))
+        
+        chosen_weight = random.uniform(0, weight_sum)
+        for cum_weight, item in cum_weight_items:
+            if chosen_weight <= cum_weight: break
+        
+        str = str[:match.start()] + item + str[match.end():]
+    return str
+
+#===============================================================================
+ABBREV_MAX_LEN = 50
+def abbrev_middle(str):
+    if len(str) <= ABBREV_MAX_LEN: return str
+    left_len = (ABBREV_MAX_LEN - 3)/2
+    right_len = (ABBREV_MAX_LEN - 3) - left_len
+    return str[:left_len] + '...' + str[-right_len:]
 
 #===============================================================================
 def roll_str_int(dice, sides, add):
