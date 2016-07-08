de87180dcfcc04510dad982cd76b6d919f4caeeb
joo <john.macinroe@gmail.com>
2016-07-08 14:13:28 +0100

    Extend 'qdbs' with public notifications.

--- page/qdbs.py
+++ page/qdbs.py
@@ -6,6 +6,7 @@ import urllib
 import urllib2
 import json
 import re
+import UserDict
 
 from bs4 import BeautifulSoup
 from untwisted.magic import sign
@@ -15,7 +16,8 @@ import runtime
 import identity
 
 STATE_FILE = 'state/qdbs.json'
-CONF_FILE = 'conf/qdbs.py'
+MAX_REPORT = 3
+MAX_QUOTE_LEN = 300
 TICK_PERIOD_S = 60
 
 link = util.LinkSet()
@@ -25,15 +27,6 @@ def install(bot):
 install, uninstall = util.depend(install, link.uninstall, 'identity')
 
 #===============================================================================
-def read_conf():
-    try:
-        return util.table(CONF_FILE, 'qdbs_conf_entry')
-    except IOError as e:
-        if e.errno != 2: raise
-    except:
-        traceback.print_exc()
-    return dict()
-
 def read_state():
     try:
         with open(STATE_FILE, 'r') as file:
@@ -48,65 +41,189 @@ def write_state(wstate):
     with open(STATE_FILE, 'w') as file:
         json.dump(wstate, file)
 
-conf = read_conf()
 state = read_state()
 
 #===============================================================================
 @link('QDBS_TICK')
 def h_qdbs_tick(bot, log_level=None):
-    for entry in conf:
-        yield refresh_conf_entry(bot, entry)
+    yield Private.class_refresh(bot)
+    yield Public.class_refresh(bot)
     yield runtime.sleep(TICK_PERIOD_S)
     yield sign('QDBS_TICK', bot, log_level=log_level)
 
-@util.msub(link, 'qdbs.refresh_conf_entry')
-def refresh_conf_entry(bot, entry):
-    try:
-        nicks = yield identity.enum_access(bot, entry.access_name)
-        if not nicks: return
-        quotes = conf_entry_quotes(entry)
-        for nick in nicks:
-            url_state = state.get(entry.admin_url, {})
-            name_state = url_state.get(entry.access_name.lower(), {})
-            last_quote = name_state.get('last_quote')
+def format_quote(quote):
+    quote = re.sub(r'[\r\n]+', ' ', quote)
+    if len(quote) > MAX_QUOTE_LEN:
+        quote = quote[:MAX_QUOTE_LEN-5] + '(...)'
+    return quote
 
-            for qid, quote in sorted(quotes):
-                if qid > last_quote:
-                    quote = quote.encode('utf-8')
-                    msg = '[QdbS] New quote #%d: %r' % (qid, quote)
-                    bot.send_msg(nick, msg)
+#===============================================================================
+class EmptyDict(UserDict.DictMixin):
+    def __getitem__(self, key):
+        raise KeyError
+    def __delitem__(self, key):
+        pass
+    def __setitem__(self, key, val):
+        pass
+
+class Configuration(object):
+    cache = EmptyDict()
+
+    def __init__(self, raw_entry):
+        for key, val in raw_entry.__dict__.iteritems():
+            setattr(self, key, val)
+
+    @classmethod
+    def read_conf(cls):
+        try:
+            return util.table(cls.CONF_FILE, cls.__name__ + 'ConfEntry')
+        except IOError as e:
+            if e.errno != 2: raise
+        except:
+            traceback.print_exc()
+        return dict()
+
+    @classmethod
+    def class_init(cls):
+        cls.cache = dict()
+        cls.entries = []
+        for raw_entry in cls.read_conf():
+            entry = cls(raw_entry)
+            cls.entries.append(entry)
+
+    @classmethod
+    @util.msub(link, 'qdbs.Configuration.class_refresh')
+    def class_refresh(cls, bot):
+        cls.cache.clear()
+        for entry in cls.entries:
+            yield entry.refresh(bot)
+
+#===============================================================================
+class Private(Configuration):
+    CONF_FILE = 'conf/qdbs_private.py'
+    __slots__ = ('access_name', 'qdb_username', 'qdb_password', 'admin_url',
+                 'remote_admin_url')
+
+    @util.msub(link, 'qdbs.Private.refresh')
+    def refresh(self, bot):
+        try:
+            nicks = yield identity.enum_access(bot, self.access_name)
+            if not nicks: return
+            quotes = self.quotes()
+            for nick in nicks:
+                url_state = state.get(self.admin_url, {})
+                name_state = url_state.get(self.access_name.lower(), {})
+                last_quote = name_state.get('last_quote')
+    
+                for qid, quote in sorted(quotes):
+                    if qid > last_quote:
+                        msg = '[QdbS] New quote #%d <%s>: %s' % (
+                            qid, self.remote_admin_url, format_quote(quote))
+                        bot.send_msg(nick, msg)
+    
+                if quotes:
+                    last_quote = max(
+                        last_quote, max(qid for (qid, quote) in quotes))
+                    name_state['last_quote'] = last_quote
+                    url_state[self.access_name.lower()] = name_state
+                    state[self.admin_url] = url_state
+                    write_state(state)
+        except:
+            traceback.print_exc()
+    
+    def quotes(self):
+        if self.admin_url in self.cache:
+            return self.cache[self.admin_url]
+        
+        quotes = []
+        soup = self.soup()
+        add_quotes = soup.find_all('a', {'title': 'Add Quote'})
+        if add_quotes:
+            for add_quote in add_quotes:
+                qid = int(re.search(r'q=(\d+)', add_quote.get('href')).group(1))
+                body = add_quote.find_parent('table').find('td', {'class': 'body'})
+                quotes.append((qid, body.text.strip()))
+        else:
+            for tag in soup.find_all('td', {'class': 'title'}):
+                if tag.text.strip() == 'There are no new quotes!':
+                    break
+            else:
+                print(soup.encode('utf-8'), file=sys.stderr)
+                raise Exception('Unexpected QdbS admin page format.')   
+
+        self.cache[self.admin_url] = quotes
+        return quotes
+    
+    def soup(self):
+        stream = urllib2.urlopen(urllib2.Request(self.admin_url, headers={
+            'Cookie': 'qdb_username=%s; qdb_password=%s' % (
+                self.qdb_username, self.qdb_password)}))
+        encoding = stream.info().getparam('charset')
+        return BeautifulSoup(stream, 'html5lib', from_encoding=encoding)
+
+Private.class_init()
+
+#===============================================================================
+class Public(Configuration):
+    CONF_FILE = 'conf/qdbs_public.py'
+    __slots__ = ('channel', 'index_url', 'remote_index_url')
+
+    @util.msub(link, 'qdbs.Public.refresh')
+    def refresh(self, bot):
+        try:
+            quotes, title = self.quotes_title()
+            url_state = state.get(self.index_url, {})
+            chan_state = url_state.get(self.channel.lower(), {})
+            last_quote = chan_state.get('last_quote')
+
+            quotes = sorted(
+                (qid, quote) for (qid, quote) in quotes if qid > last_quote)
+            for qid, quote in quotes[:MAX_REPORT]:
+                quote_url = '%s?%s' % (self.remote_index_url, qid)
+                msg = '%s: new quote added: %s "%s"' % (
+                    title, quote_url, format_quote(quote))
+                bot.send_msg(self.channel, msg)
+
+            if len(quotes) > MAX_REPORT:
+                msg = '%s: ...and %d other(s). See: <%s>.' % (
+                    title, len(quotes)-MAX_REPORT, self.remote_index_url)
+                bot.send_msg(self.channel, msg)
 
             if quotes:
                 last_quote = max(
                     last_quote, max(qid for (qid, quote) in quotes))
-                name_state['last_quote'] = last_quote
-                url_state[entry.access_name.lower()] = name_state
-                state[entry.admin_url] = url_state
+                chan_state['last_quote'] = last_quote
+                url_state[self.channel.lower()] = chan_state
+                state[self.index_url] = url_state
                 write_state(state)
-    except:
-        traceback.print_exc()
+        except:
+            traceback.print_exc()
 
-def conf_entry_quotes(entry):
-    quotes = []
-    soup = conf_entry_soup(entry)
-    add_quotes = soup.find_all('a', {'title': 'Add Quote'})
-    if add_quotes:
-        for add_quote in add_quotes:
-            qid = int(re.search(r'q=(\d+)', add_quote.get('href')).group(1))
-            body = add_quote.find_parent('table').find('td', {'class': 'body'})
-            quotes.append((qid, body.text.strip()))
-    else:
-        for tag in soup.find_all('td', {'class': 'title'}):
-            if tag.text.strip() == 'There are no new quotes!':
-                break
-        else:
+    def quotes_title(self):
+        if self.index_url in self.cache:
+            return self.cache[self.index_url]
+
+        quotes = []
+        soup = self.soup()
+
+        heading = soup.find('td', {'class': 'heading'})
+        if heading is None:
             print(soup.encode('utf-8'), file=sys.stderr)
-            raise Exception('Unexpected QdbS admin page format.')   
-    return quotes
+            raise Exception('Unexpected QdbS index page format.')
+        qdb_title = heading.text.strip()
 
-def conf_entry_soup(entry):
-    stream = urllib2.urlopen(urllib2.Request(entry.admin_url, headers={
-        'Cookie': 'qdb_username=%s; qdb_password=%s' % (
-            entry.qdb_username, entry.qdb_password)}))
-    encoding = stream.info().getparam('charset')
-    return BeautifulSoup(stream, 'html5lib', from_encoding=encoding)
+        rate_links = soup.find_all('a', {'title': 'Rate as good'})
+        for rate_link in rate_links:
+            qid = int(re.search(r'q=(\d+)', rate_link.get('href')).group(1))
+            body = rate_link.find_parent('table').find('td', {'class': 'body'})
+            quotes.append((qid, body.text.strip()))
+
+        self.cache[self.index_url] = quotes, qdb_title
+        return quotes, qdb_title
+    
+    def soup(self):
+        stream = urllib2.urlopen(self.index_url)
+        encoding = stream.info().getparam('charset')
+        return BeautifulSoup(stream, 'html5lib', from_encoding=encoding)
+
+Public.class_init()
