e0aeca6738b43d5e8ed30ce53f9922690f3267aa
joo <john.macinroe@gmail.com>
2016-08-25 23:00:47 +0100

    Various improvements to the heuristics of 'flood'.

--- page/flood.py
+++ page/flood.py
@@ -1,26 +1,32 @@
 from collections import defaultdict
 from collections import namedtuple
+from itertools import *
 import time
 import array
 import re
 import os.path
 
 import util
+import channel
 link, install, uninstall = util.LinkSet().triple()
 
 CONF_CHANS_FILE = 'conf/flood_chans.py'
 
 HISTORY_SECONDS = 3600
 HISTORY_ENTRIES = 250
-SCORE_MUL_HOUR  = 0.1
-SCORE_MUL_10MSG = 0.25
-SCORE_POLE_SEC  = 1.5
-SCORE_POLE_MAX  = 2
-SCORE_MUL_OTHER = 0.5
-SCORE_MIN_SELF  = 0.1
-SCORE_THRESHOLD = 5
+SCORE_MUL_HOUR = 0.1
+SCORE_MUL_10MSG = 0.5
+SCORE_MUL_NICKS = 3.0
+SCORE_MUL_UPPER = 1.5
+SCORE_POLE_SEC = 1.5
+SCORE_POLE_MAX = 2.0
+SCORE_MUL_OTHER = 0.7
+SCORE_MIN_SELF = 0.1
+MIN_EFFECTIVE_LEN = 20
+MIN_EFFECTIVE_NICKS = 10
+SCORE_THRESHOLD = 3.0
 
-Msg = namedtuple('Msg', ('time', 'id', 'text'))
+Msg = namedtuple('Msg', ('time', 'id', 'text', 'score'))
 
 # chan_history[chan.lower()] = [Msg(...), Msg(...), ...]
 chan_history = defaultdict(list)
@@ -48,8 +54,7 @@ def h_message(bot, id, chan, text):
     chan = chan.lower()
     if chan not in conf_chans: return
 
-    score = handle_msg(Msg(time.time(), id, text), chan)
-    print('*** score=%.04f' % score)
+    score = handle_msg(Msg(time.time(), id, text, 0), chan)
     if score > SCORE_THRESHOLD: punish(bot, id, chan)
 
 def punish(bot, id, chan):
@@ -70,29 +75,46 @@ def handle_msg(msg, chan):
     del history[:len(history)-HISTORY_ENTRIES]
     while history and msg.time - history[0].time > HISTORY_SECONDS:
         del history[0]
-    score = score_msg(msg, history)
-    history.append(msg)
+    score = score_msg(msg, chan, history)
+    history.append(msg._replace(score=score))
     chan_history[chan] = history   
     return score
 
-def score_msg(msg, history):
-    score = 0
+def score_msg(msg, chan, history):
+    score = 0.0
     for i in xrange(1, len(history)+1):
         hmsg = history[-i]
-        score += score_msg_part(msg, hmsg, i)
+        part = score_msg_part(msg, hmsg, rel_num=i)
+        score += part
+
+    nicks = channel.track_channels[chan]
+    mnicks = sum(1 for n in nicks if re.search(re.escape(n), msg.text, re.I))
+    score *= SCORE_MUL_NICKS**(float(mnicks)/max(MIN_EFFECTIVE_NICKS, len(nicks)))
+
+    uchrs = sum(1 for c in msg.text if c.isupper())
+    score *= SCORE_MUL_UPPER**(float(uchrs)/max(MIN_EFFECTIVE_LEN, len(msg.text)))
+
     return score
 
-def score_msg_part(msg, hmsg, i):
-    tdif = msg.time - hmsg.time
-    part = similarity(msg.text, hmsg.text)**2
+def score_msg_part(msg, hmsg, rel_num):
+    part = max(1.0, min(2.0, hmsg.score))
+
+    min_eff_len = MIN_EFFECTIVE_LEN*(1 - 1/rel_num**2)
+    part *= similarity(msg.text, hmsg.text, min_eff_len)**2
+
     if same_user(msg.id, hmsg.id):
         part = max(SCORE_MIN_SELF, part)
     else:
         part *= SCORE_MUL_OTHER
+
+    part *= SCORE_MUL_10MSG**(rel_num/10.0)
+
+    tdif = msg.time - hmsg.time
     part *= SCORE_MUL_HOUR**(tdif/3600.0)
+
     tmin = SCORE_POLE_SEC/(SCORE_POLE_MAX - 1)
     part *= (1 + SCORE_POLE_SEC/max(tmin, tdif))
-    part *= SCORE_MUL_10MSG**(i/10.0)
+
     return part
 
 def same_user(id1, id2):
@@ -100,10 +122,11 @@ def same_user(id1, id2):
            (id1.user == id2.user or id1.user[:1] == id2.user[:1] == '~')
 
 WORD_RE = re.compile(r'(\w+)', re.U)
-def similarity(str1, str2):
-    str1, str2 = WORD_RE.split(str1), WORD_RE.split(str2)
-    lcs = longest_common_substring(str1, str2)
-    return float(len(lcs)) / float(max(len(str1), len(str2)))
+def similarity(str1, str2, min_eff_len):
+    str1w, str2w = WORD_RE.split(str1), WORD_RE.split(str2)
+    lcs_len = sum(len(w) for w in longest_common_substring(str1w, str2w))
+    ratio = float(lcs_len+1)/(max(len(str1), len(str2), min_eff_len) + 1)
+    return ratio
 
 def longest_common_substring(str1, str2):
     len1, len2 = len(str1), len(str2)
--- tools/test_flood.py
+++ tools/test_flood.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python2
-
 from __future__ import print_function
 
 from datetime import datetime
+from itertools import *
+import os.path
 import sys
 import re
 import time
-import cProfile
 
-import main
+sys.path[:0] = [
+    os.path.join(os.path.dirname(__file__), '../ameliabot'),
+    os.path.join(os.path.dirname(__file__), '../lib'),
+    os.path.join(os.path.dirname(__file__), '../page')]
+
 import util
 import flood
+import channel
+
+CHAN_NAME = ('#%s' % os.path.basename(__name__)).lower()
 
 UNKNOWN   = 0
 UNCERTAIN = 1
@@ -20,6 +27,7 @@ FLOODING  = 3
 def main():
     date = None
     nicks = dict()
+    prev_time = None
     
     for line in sys.stdin:
         line = re.sub(r'\n$', '', line)
@@ -33,28 +41,42 @@ def main():
             if date is not None:
                 time = datetime.combine(date, time)
                 time = (time - datetime(1970, 1, 1)).total_seconds()
+                while time <= prev_time: time += 1
+                prev_time = time
             else:
                 time = None
     
             match = re.match(r'<[ @+]?(?P<nick>\S+)> (?P<msg>.*)', body)
             if match:
                 nick, msg = match.group('nick', 'msg')
+                track(nick)
                 if nick in nicks and time is not None:
-                    handle_msg(time, nicks[nick], msg)
-                    status = NORMAL
+                    status = handle_msg(time, nicks[nick], msg)
                 else:
                     status = UNCERTAIN
     
             match = re.match(
-            r'-!- (?P<nick>\S+) \[(?P<user>\S+)@(?P<host>\S+)\] has joined', body)
+            r'-!- (?P<nick>\S+) \[(?P<user>\S+)@(?P<host>\S+)\] has '
+            '(?P<action>joined|quit|left)', body)
             if match:
-                nicks[match.group('nick')] = util.ID(**match.groupdict())
-                status = NORMAL
+                nick, user, host, action = match.groups()
+                if action == 'joined':
+                    track(nick)
+                    nicks[nick] = util.ID(nick, user, host)
+                    status = NORMAL
+                elif action in ('quit', 'left'):
+                    untrack(nick)
+
+            match = re.match(r'-!- (?P<nick>\S+) was kicked from', body)
+            if match:
+                untrack(nick)
     
             match = re.match(
             r'-!- (?P<old_nick>\S+) is now known as (?P<new_nick>\S+)$', body)
             if match:
                 old_nick, new_nick = match.group('old_nick', 'new_nick')
+                untrack(old_nick)
+                track(new_nick)
                 if old_nick in nicks:
                     nicks[new_nick] = nicks.pop(old_nick)
                 status = NORMAL
@@ -72,17 +94,36 @@ def main():
         elif line.startswith('--- Log closed'):
             date = None
             status = NORMAL
+            if CHAN_NAME in channel.track_channels:
+                del channel.track_channels[CHAN_NAME]
 
-        code = { NORMAL:39, UNKNOWN:36, UNCERTAIN:35, FLOODING:31 }[status]
-        print('\033[%(code)dm%(line)s\033[0m' % {'code':code, 'line':line})
+        code = {
+            NORMAL:39, UNKNOWN:36, UNCERTAIN:35, FLOODING:31
+        }[status]
+        print('\033[%(code)sm%(line)s\033[0m' % {'code':code, 'line':line})
+
+def track(nick):
+    nick = nick.lower()
+    nicks = channel.track_channels[CHAN_NAME]
+    if nick not in nicks: nicks.append(nick)
+    channel.track_channels[CHAN_NAME] = nicks
+
+def untrack(nick):
+    nick = nick.lower()
+    nicks = channel.track_channels[CHAN_NAME]
+    if nick in nicks: nicks.remove(nick)
+    channel.track_channels[CHAN_NAME] = nicks    
 
 def handle_msg(time, id, text):
-    msg = flood.Msg(time=time, id=id, text=text)
-    score = flood.handle_msg(msg, id, '#test_flood.py')
-    code, char = ('31;1', '!') if score > flood.SCORE_THRESHOLD \
-            else ('31', ' ')
+    msg = flood.Msg(time=time, id=id, text=text, score=0)
+    score = flood.handle_msg(msg, CHAN_NAME)
+    if score > flood.SCORE_THRESHOLD:
+        code, char, status = '31;1', '!', FLOODING
+    else:
+        code, char, status = '31',   ' ', NORMAL
     print('\033[%(code)sm%(score).01f%(char)s\033[0m'
         % {'code':code, 'score':score, 'char':char}, end='')
+    return status
     
 if __name__ == '__main__':
     main()
