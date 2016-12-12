d3277650bbc8473b9ccb61a2841a337134831c3d
joo <john.macinroe@gmail.com>
2016-12-12 02:57:22 +0000

    dungeonworld: add !view-missed-rolls and !clear-missed-rolls commands.

--- page/dungeonworld.py
+++ page/dungeonworld.py
@@ -3,6 +3,7 @@ from collections import Counter
 from datetime import datetime
 
 from util import multi
+import channel
 import pastebin
 import message
 import util
@@ -16,66 +17,66 @@ LOG_FILE = 'state/dungeonworld_rolls.txt'
 #===============================================================================
 @link('HELP')
 def h_help(bot, reply, args):
-    reply('missed-rolls',
-    'Gives a tally of missed 2d6+N rolls.')
+    reply('missed-rolls', 'Gives a tally of missed 2d6+N rolls.')
 
-#-------------------------------------------------------------------------------
 @link(('HELP', 'missed-rolls'))
 def h_help_missedrolls(bot, reply, args):
     reply('missed-rolls',
-    'Gives a tally of dice rolls of the form 2d6+N whose value was less than 7,'
-    ' grouped by the nick who made the roll, since the last time this command'
-    ' was invoked. This is useful for playing Dungeon World RPGs in IRC.')
+    'Gives a tally of dice rolls of the form 2d6+N, b2(Md6+N) or w2(Md6+N)'
+    ' whose result was less than 7, grouped by the nick who made the roll,'
+    ' since the last time this command was invoked. This is useful for'
+    ' playing Dungeon World RPGs in IRC. See also: \2!view-missed-rolls\2,'
+    ' \2!insert-missed-roll\2, \2!delete-missed-roll\2,'
+    ' \2!clear-missed-rolls\2.')
 
-#-------------------------------------------------------------------------------
 @link('!missed-rolls')
 def h_missed_rolls(bot, id, target, args, full_msg):
-    reply = lambda m: message.reply(bot, id, target, m, prefix=False, wrap=True)
-    try: all_rolls = util.read_list(LOG_FILE)
-    except IOError: all_rolls = []
-    rolls = filter(lambda (f,m,t,i,r): t.lower()==target.lower(), all_rolls)
-
-    nick_case = dict()
-    nick_move, nick_fail = Counter(), Counter()
-    for (f,m,t,(n,u,h),r) in rolls:
-        n_key = n.lower()
-        if n_key not in nick_case: nick_case[n_key] = n
-        nick_fail[n_key] += f
-        nick_move[n_key] += m
-
-    if not nick_fail: return reply('Missed rolls: none.')
-    
-    nicks_text = ', '.join(
-        '\2%s: %d\2 of %d' % (nick_case[n], f, nick_move[n])
-        for (n,f) in nick_fail.iteritems())
-    reply_msg = 'Missed rolls (estimated): %s.' % nicks_text
-
-    rolls_text = ''.join(
-        '%s (%d/%d)%s\n' % (nick_case[n.lower()], f, m, r and ': %s' % r)
-        for (f,m,t,(n,u,h),r) in rolls if f)
-    if rolls_text:
-        time = datetime.now().strftime('%Y-%m-%d %H:%M')
-        rolls_url = pastebin.post(
-            strip_irc(rolls_text),
-            paste_expire = pastebin.E_1MONTH,
-            paste_name   = '%s %s Missed Rolls %s' % (bot.nick, target, time))
-        reply_msg += ' Full list: <%s>.' % rolls_url
-
-    reply(reply_msg)
-
-    all_rolls = filter(lambda r: r not in rolls, all_rolls)
-    with open(LOG_FILE, 'w') as file:
-        for roll in all_rolls: print(roll, file=file)
+    if target is None:
+        message.reply(bot, id, target,
+            'Error: this command may not be used by PM; however, see'
+            ' \2!help view-missed-rolls\2 for a version which can.')
+    elif args:
+        message.reply(bot, id, target,
+            'Error: this command does not accept any parameters. Perhaps you'
+            ' meant \2!view-missed-rolls\2.')
+    else:
+        def reply(msg):
+            message.reply(bot, id, target, msg, prefix=False, wrap=True)
+        yield show_missed_rolls(bot, target, reply, delete=True)
 
 #-------------------------------------------------------------------------------
-'''
-@link('HELP')
-def h_help(bot, reply, args):
-    reply('insert-missed-roll [NICK [COMMENT ...]]',
-    'Inserted a synthetic missed dice roll.')
-'''
+@link(('HELP', 'view-missed-rolls'))
+def h_help_insert_missed_roll(bot, reply, args):
+    reply('view-missed-rolls [CHANNEL]',
+    'Gives a tally of missed dice rolls in CHANNEL, or in the current channel'
+    ' if not specified, as per \2!missed-rolls\2, without deleting any rolls'
+    ' from the record. May be used by PM.')
 
-@link(('HELP', 'missed-rolls'))
+@link('!view-missed-rolls')
+def h_view_missed_rolls(bot, id, target, args, full_msg):
+    if args:
+        nicks = map(str.lower, channel.track_channels[args.lower()])
+        if id.nick.lower() not in nicks:
+            message.reply(bot, id, target,
+                'Error: you must be in "%s" to view its rolls.' % args)
+            return
+        chan = args
+        local = False
+    elif not target:
+        message.reply(bot, id, target,
+            'Error: you must specify a channel. See'
+            ' \2!help view-missed-rolls\2 for correct usage.')
+        return
+    else:
+        chan = target
+        local = True
+
+    def reply(msg):
+        message.reply(bot, id, target, msg, prefix=False, wrap=True)
+    yield show_missed_rolls(bot, chan, reply, delete=False, local=local)
+
+#-------------------------------------------------------------------------------
+@link(('HELP', 'insert-missed-roll'))
 def h_help_insert_missed_roll(bot, reply, args):
     reply('insert-missed-roll [NICK [COMMENT ...]] [!insert-missed-roll ...]',
     'Inserts into the record of dice rolls an entry meaning that NICK (or the'
@@ -91,20 +92,11 @@ def h_insert_missed_roll(bot, id, target, args, full_msg, reply):
     r_id = (nick,'*','*') if nick else id
     label = label or '(inserted by %s)' % id.nick
     
-    with open(LOG_FILE, 'a') as file:
-        print((1,1,target,r_id,label), file=file)
-
+    append_roll((1, 1, target, r_id, label))
     reply('Inserted 1 missed roll by %s.' % r_id[0])
 
 #-------------------------------------------------------------------------------
-'''
-@link('HELP')
-def h_help(bot, reply, args):
-    reply('delete-missed-roll [NICK]',
-    'Deletes the last missed dice roll.')
-'''
-
-@link(('HELP', 'missed-rolls'))
+@link(('HELP', 'delete-missed-roll'))
 def h_help_insert_missed_roll(bot, reply, args):
     reply('delete-missed-roll [NICK] [!delete-missed-roll ...]',
     'Deletes from the record of dice rolls the last roll by NICK (or, if not'
@@ -116,9 +108,7 @@ def h_delete_missed_roll(bot, id, target, args, full_msg, reply):
     if not target: return
     nick = re.match(r'\S*', args).group()
 
-    try: all_rolls = util.read_list(LOG_FILE)
-    except IOError: all_rolls = []
-
+    all_rolls = read_rolls()
     delete_index = None
     for index in range(len(all_rolls)):
         (failed,_,r_target,(r_nick,_,_),_) = all_rolls[index]
@@ -131,13 +121,85 @@ def h_delete_missed_roll(bot, id, target, args, full_msg, reply):
         'There are no missed rolls%s to delete.' % (nick and ' by "%s"' % nick))
 
     (_,_,_,r_id,label) = all_rolls.pop(delete_index)
-
-    with open(LOG_FILE, 'w') as file:
-        for roll in all_rolls:
-            print(roll, file=file)
-
+    replace_rolls(all_rolls)
     reply('Deleted %d missed roll%s by %s%s.' % (
         failed, 's' if failed != 1 else '', r_id[0], label and ': "%s"' % label))
+
+#-------------------------------------------------------------------------------
+@link(('HELP', 'clear-missed-rolls'))
+def h_help_insert_missed_roll(bot, reply, args):
+    reply('clear-missed-rolls',
+    'Deletes all dice rolls recorded in this channel for the purposes of'
+    ' \2!missed-rolls\2.')
+
+@link('!clear-missed-rolls')
+def h_clear_missed_rolls(bot, id, target, args, full_msg):
+    if not target: return
+    if args:
+        message.reply(bot, id, target,
+            'Error: this command does not accept any parameters.'
+            ' See \2!help clear-missed-rolls\2 for correct usage.')
+        return
+
+    all_rolls = read_rolls()
+    move_count, fail_count, new_rolls = 0, 0, []
+    for roll in all_rolls:
+        (r_fail, r_move, r_target, _, _) = roll
+        if r_target.lower() == target.lower():
+            move_count += r_move
+            fail_count += r_fail
+        else:
+            new_rolls.append(roll)        
+
+    replace_rolls(new_rolls)
+    message.reply(bot, id, target,
+        'Deleted %d dice rolls, of which %d were missed.'
+        % (move_count, fail_count))
+
+#-------------------------------------------------------------------------------
+@util.msub(link, 'dungeonworld.show_missed_rolls')
+def show_missed_rolls(
+    bot, target, reply, delete=True, all_rolls=True, local=True
+):
+    all_rolls = read_rolls()
+    rolls = filter(lambda (f,m,t,i,r): t.lower()==target.lower(), all_rolls)
+
+    nick_case = dict()
+    nick_move, nick_fail = Counter(), Counter()
+    for (f,m,t,(n,u,h),r) in rolls:
+        n_key = n.lower()
+        if n_key not in nick_case: nick_case[n_key] = n
+        nick_fail[n_key] += f
+        nick_move[n_key] += m
+
+    reply_msg = 'Missed rolls%s (estimated): ' % ('' if local else
+        ' in %s' % channel.capitalisation.get(target, target),)
+
+    if nick_fail:
+        nicks_text = ', '.join(
+            '\2%s: %d\2 of %d' % (nick_case[n], f, nick_move[n])
+            for (n,f) in nick_fail.iteritems())
+        reply_msg += '%s.' % nicks_text
+    else:
+        reply_msg += 'none.'
+
+    if nick_move and (all_rolls or nick_fail):
+        rolls_text = ''.join(
+            '%s (%d/%d)%s\n' % (nick_case[n.lower()], f, m, r and ': %s' % r)
+            for (f,m,t,(n,u,h),r) in rolls if all_rolls or f)
+        if rolls_text:
+            time = datetime.now().strftime('%Y-%m-%d %H:%M')
+            rolls_url = pastebin.post(
+                strip_irc(rolls_text),
+                paste_expire=pastebin.E_1MONTH,
+                paste_name='%s %s Missed Rolls %s' % (bot.nick, target, time))
+            reply_msg += ' Full list: <%s>.' % rolls_url
+
+    reply(reply_msg)
+
+    if delete:
+        all_rolls = filter(lambda r: r not in rolls, all_rolls)
+        replace_rolls(all_rolls)
 
 #-------------------------------------------------------------------------------
 @link('DICE_ROLLS')
@@ -145,18 +207,27 @@ def h_dice_rolls(bot, id, target, rolls, result_msg):
     move_rolls = filter(is_move_roll, rolls)
     fail_rolls = filter(lambda (t,r): r < 7, move_rolls)
     if not move_rolls or not target: return
-    with open(LOG_FILE, 'a') as file:
-        print((
-            len(fail_rolls),
-            len(move_rolls),
-            target,
-            tuple(id),
-            result_msg
-        ), file=file)
+    append_roll((
+        len(fail_rolls),
+        len(move_rolls),
+        target,
+        tuple(id),
+        result_msg))
 
 def is_move_roll(((d,s,a), r)):
     dice, drop_low, drop_high = d if isinstance(d, tuple) else (d, 0, 0)
     return (dice - drop_low - drop_high, s) == (2, 6)
+
+def read_rolls():
+    return util.read_list(LOG_FILE, default=True)
+
+def replace_rolls(rolls):
+    with open(LOG_FILE, 'w') as file:
+        for roll in rolls: print(roll, file=file)
+
+def append_roll(roll):
+    with open(LOG_FILE, 'a') as file:
+        print(roll, file=file)
 
 #===============================================================================
 def strip_irc(str):
